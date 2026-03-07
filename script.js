79cc67406db8dbe73593e1999b03233d5069bcb7
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-07 19:16:49 +0300

    выполнил дз для первого урока

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = '';
+const screens = '';
+const screenPrice = '';
+const rollback = '';
+const fullPrice = '';
+const adaptive = '';
+
+alert('Сообщение с любым текстом');
+console.log('Сообщение с любым текстом в консоли');
+// Все работает, ошибок в консоли нет
