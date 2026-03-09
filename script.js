e6a07bba7b840106ac509d459ae6d7e23747a974
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-09 16:34:40 +0300

    добавил закрывающие ;

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const rollback = 89;
 const fullPrice = 500000;
 const adaptive = true;
 
-console.log(typeof title, typeof fullPrice, typeof adaptive)
-console.log(screens.length)
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
-screens.toLowerCase()
-console.log(screens.split(', '))
-console.log(fullPrice * (rollback / 100))
-console.log(fullPrice * (rollback / 100) > 10000 ? 'Обдиралово' : 'По рукам')
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+screens.toLowerCase();
+console.log(screens.split(', '));
+console.log(fullPrice * (rollback / 100));
+console.log(fullPrice * (rollback / 100) > 10000 ? 'Обдиралово' : 'По рукам');
