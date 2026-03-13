83d5de99f89cab856b82fbab0a048c8ed454e18c
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-13 22:27:22 +0300

    мелкие правки

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const title = 'learn JavaScript';
-const screens = 'Простые, Сложные, Интерактивные';
+const title = "learn JavaScript";
+const screens = "Простые, Сложные, Интерактивные";
 const screenPrice = 382750;
 const rollback = 89;
 const fullPrice = 500000;
@@ -10,6 +10,6 @@ console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 screens.toLowerCase();
-console.log(screens.split(', '));
+console.log(screens.split(", "));
 console.log(fullPrice * (rollback / 100));
-console.log(fullPrice * (rollback / 100) > 10000 ? 'Обдиралово' : 'По рукам');
+console.log(fullPrice * (rollback / 100) > 10000 ? "Обдиралово" : "По рукам");
