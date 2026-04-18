1bd94ab308d133ef1d5f50064497c3791825bef4
Vyacheslav <slavaslava.s@yandex.ru>
2026-04-18 20:04:54 +0300

    ДЗ в процессе

--- you-dont-know-js/index.js
+++ you-dont-know-js/index.js
@@ -1,1 +1,17 @@
 "use script";
+
+let booksItem = document.querySelectorAll(".book");
+let adBanner = document.querySelector(".adv");
+
+booksItem[0].before(booksItem[1]);
+booksItem = document.querySelectorAll(".book");
+booksItem[2].before(booksItem[4]);
+booksItem = document.querySelectorAll(".book");
+booksItem[5].after(booksItem[3]);
+booksItem = document.querySelectorAll(".book");
+let thirdBook = booksItem[2].querySelector("a");
+
+adBanner.style.display = "none";
+document.body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+
+thirdBook.textContent = "Книга 3. this и Прототипы Объектов";
